63164efc602163b0e67e1018a05ff95054a2b338
ishaan-bhandari <ishaanbhandari0@gmail.com>
2024-06-26 20:37:02 -0500

    updated app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,10 +5,120 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 import requests
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai
+from dotenv import load_dotenv
+import os
+import shutil
+
+import argparse
+# from dataclasses import dataclass
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+CHROMA_PATH = "chroma"
 
 transcript_list = []
+transcript = ""
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def generate_data_store():
+    transcript = " ".join(transcript_list)
+    documents = Document(page_content=transcript)
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+
+def split_text(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
+
+
+def save_to_chroma(chunks):
+    # Clear out the database first.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+
+@app.route('/test')
+def test():
+    print(request.headers)
+
+    query_text = request.args.get('query_text')
+    print(query_text)
+    return "hello bitch"
+
+
+@app.route('/query_rag_model', methods=['GET'])
+def query_rag_model():
+
+    print(request.headers)
+
+    query_text = request.args.get('query_text')
+
+    print("here", query_text)
+
+    embedding_function = OpenAIEmbeddings()
+
+    db = Chroma(persist_directory=CHROMA_PATH,
+                embedding_function=embedding_function)
+
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    print("here2", results)
+
+    context_text = "\n\n---\n\n".join(
+        [doc.page_content for doc, _score in results])
+
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    print(prompt)
+    return jsonify(response_text), 200
 
 
 def preprocess_text(text):
@@ -39,6 +149,7 @@ def segment_transcript(data, num_questions):
     if current_segment:
         segments.append(' '.join(current_segment))
         end_times.append(word['e'] // 1000)
+    generate_data_store()
     return segments, end_times
 
 
